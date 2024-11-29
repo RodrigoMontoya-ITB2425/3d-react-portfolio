6c3e157ce1d7bca0fd9d4be4cede3a9ed107f2fb
RodrigoMontoya-ITB2425 <rodrigo.montoya.7e8@itb.cat>
2024-11-29 23:44:31 +0100

    ugbu

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
-    },
-    base: "/3d-react-portfolio",
 })
