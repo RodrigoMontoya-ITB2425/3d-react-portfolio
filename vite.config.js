9f7028309ef585d78c1b68dce355c096a597372a
RodrigoMontoya-ITB2425 <rodrigo.montoya.7e8@itb.cat>
2024-11-30 05:34:22 +0100

    jb

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr' // Importa el plugin svgr
+import postcssPresetEnv from 'postcss-preset-env' // Asegúrate de importar esto de manera explícita
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,13 +19,16 @@ export default defineConfig({
     })
   ],
   build: {
-    chunkSizeWarningLimit: 100000000
+    chunkSizeWarningLimit: 100000000,
+    // Si estás construyendo para un proyecto en una subcarpeta, asegúrate de que la base esté configurada correctamente.
+    // Esto afectará las rutas de los assets durante la construcción.
+    outDir: 'dist', // Directorio de salida de la construcción
   },
-  base: "/3d-react-portfolio",
+  base: "/3d-react-portfolio", // Esto asegura que se sirva desde una subcarpeta específica
   css: {
     postcss: {
       plugins: [
-        require('postcss-preset-env')({
+        postcssPresetEnv({
           stage: 1, // Ajusta según lo que necesites
         }),
       ],
