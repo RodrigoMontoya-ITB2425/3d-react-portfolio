58f9a5afd7f7dbccf10399992572139ca9a3f3ce
RodrigoMontoya-ITB2425 <rodrigo.montoya.7e8@itb.cat>
2024-11-30 02:40:46 +0100

    asf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',  
+  base: '/Portfolio/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+    manifest: true, 
+  },
+});
