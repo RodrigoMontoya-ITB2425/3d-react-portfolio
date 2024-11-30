6ebd9cb4d1fceaec02e4f41efeafb9b946492672
RodrigoMontoya-ITB2425 <rodrigo.montoya.7e8@itb.cat>
2024-11-30 01:07:56 +0100

    asfda

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/RodrigoMontoya-ITB2425/',  
+  base:'/RodrigoMontoya-ITB2425/',  
   plugins: [react()]
 })
