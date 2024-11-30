b128e23f0d216109715b4d838c34257a4417c55a
RodrigoMontoya-ITB2425 <rodrigo.montoya.7e8@itb.cat>
2024-11-30 01:17:36 +0100

    khg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base:'/RodrigoMontoya-ITB2425/',  
-  plugins: [react()]
+  base: '/RodrigoMontoya-ITB2425/',  
+  plugins: [react()],
 })
