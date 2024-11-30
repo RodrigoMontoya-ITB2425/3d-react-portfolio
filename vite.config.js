5214b44a9da1d0a51684a2a243db67ebbfbb53b4
RodrigoMontoya-ITB2425 <rodrigo.montoya.7e8@itb.cat>
2024-11-30 02:51:10 +0100

    sf

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';  // Asegúrate de importar esta función correctamente
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',  // El subdirectorio para el despliegue en GitHub Pages
+  base: '/Portfolio/', // Adjust based on your project path
   plugins: [react()],
-  build: {
-    manifest: true, // Asegura que se genere el manifiesto
-    rollupOptions: {
-      input: './index.html',
-    },
-  },
-});
+})
