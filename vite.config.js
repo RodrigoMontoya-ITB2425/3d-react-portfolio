8de21c62e2acc66bca81ccb6d6e4d748fc1307fa
RodrigoMontoya-ITB2425 <rodrigo.montoya.7e8@itb.cat>
2024-11-30 04:58:19 +0100

    dfsf

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  plugins: [
-    svgr({
-      svgrOptions: {
-        throwIfNamespace: false, // Deshabilita el chequeo de namespace
-      },
-    }),
-  ],
-}
-
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  build: {
+    chunkSizeWarningLimit: 100000000
+  },
+  base: "/3d-react-portfolio",
+  css: {
+    postcss: {
+      plugins: [
+        require('postcss-preset-env')({
+          stage: 1, // Ajusta según lo que necesites
+        }),
+      ],
+    },
+  },
+});
